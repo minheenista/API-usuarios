b1acb22e1fe668a1c5b5221be86798159a37ee75
Mich <anamichelle1308@gmail.com>
2023-12-04 13:28:35 -0600

    fixed cluster

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const uri = "mongodb+srv://michifus:Youngjae333@web07.nt26l3k.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = "mongodb+srv://michifus:Youngjae333@web07.mqnxhlz.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
